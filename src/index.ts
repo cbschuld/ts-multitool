51050140edff731487743184c836c9221ac8d225
Chris Schuld <cbschuld@gmail.com>
2022-07-12 08:34:58 -0700

    reorganize package, eslint changes, readme bumps for shields

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+// text
 export { commaSeparatedString } from './text/commaSeparatedString'
+export { capitalize } from './text/capitalize'
+
+// file
+export { getExtension } from './files/getExtension'
+
+// array
+export { unique } from './array/unique'
--- src/text/index.ts
+++
@@ -1,1 +0,0 @@
-export { commaSeparatedString } from './commaSeparatedString'
